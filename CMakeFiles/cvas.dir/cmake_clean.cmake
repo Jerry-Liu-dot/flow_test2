e7e3e8bdf9337aa5c9f2074b41960d83631a4e8a
liujiarui <ljr-jerry@sjtu.edu.cn>
2021-03-08 19:14:51 +0800

    from zhao

---
+++ CMakeFiles/cvas.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvas.dir/src/car_status.cpp.o"
+  "CMakeFiles/cvas.dir/src/data.cpp.o"
+  "lib/libcvas.pdb"
+  "lib/libcvas.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
